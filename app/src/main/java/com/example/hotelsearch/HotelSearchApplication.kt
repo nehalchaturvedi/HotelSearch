63282db08cb71493b63d62a3be8b9b83ea30b762
nehalchaturvedi <nehal.chaturvedi@udayy.com>
2023-03-23 19:19:14 -0400

    Dependency Injection with Kodein

---
+++ app/src/main/java/com/example/hotelsearch/HotelSearchApplication.kt
@@ -0,0 +1,27 @@
+package com.example.hotelsearch
+
+import android.app.Application
+import com.example.hotelsearch.data.network.ApiService
+import com.example.hotelsearch.data.network.HotelsNetworkDataSource
+import com.example.hotelsearch.data.network.HotelsNetworkDataSourceImpl
+import com.example.hotelsearch.data.network.interceptors.ConnectivityInterceptor
+import com.example.hotelsearch.data.network.interceptors.ConnectivityInterceptorImpl
+import com.example.hotelsearch.data.repository.HotelsRepository
+import com.example.hotelsearch.data.repository.HotelsRepositoryImpl
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+
+class HotelSearchApplication : Application(), KodeinAware {
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@HotelSearchApplication))
+
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { ApiService(instance()) }
+        bind<HotelsNetworkDataSource>() with singleton { HotelsNetworkDataSourceImpl(instance()) }
+        bind<HotelsRepository>() with singleton { HotelsRepositoryImpl(instance()) }
+    }
+}
