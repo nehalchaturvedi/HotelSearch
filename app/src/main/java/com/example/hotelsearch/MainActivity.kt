e4875557a88b3a0b6c01a1cc017b16227f7ab65a
nehalchaturvedi <nehal.chaturvedi@udayy.com>
2023-03-23 17:25:34 -0400

    Testing API for response

--- app/src/main/java/com/example/hotelsearch/MainActivity.kt
+++ app/src/main/java/com/example/hotelsearch/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.hotelsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import com.example.hotelsearch.network.ApiService
+import com.example.hotelsearch.network.ConnectivityInterceptorImpl
+import com.example.hotelsearch.network.HotelsNetworkDataSourceImpl
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apisrevice = ApiService(ConnectivityInterceptorImpl(this.applicationContext))
+        val hotelsNetworkDataSourceImpl = HotelsNetworkDataSourceImpl(apisrevice)
+
+        hotelsNetworkDataSourceImpl.downloadedHotelsList.observe(this, Observer {
+            Log.d("response", it.toString())
+        })
+
+        GlobalScope.launch(Dispatchers.Main) {
+            hotelsNetworkDataSourceImpl.fetchHotels("New york")
+        }
     }
 }
