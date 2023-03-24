2ed9ce123760c513182aa3dcb6e47203560ec3c7
nehalchaturvedi <nehal.chaturvedi@udayy.com>
2023-03-24 00:34:24 -0400

    Linking listeners

--- app/src/main/java/com/example/hotelsearch/ui/hotelsearch/HotelSearchActivity.kt
+++ app/src/main/java/com/example/hotelsearch/ui/hotelsearch/HotelSearchActivity.kt
@@ -3,9 +3,11 @@ package com.example.hotelsearch.ui.hotelsearch
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.hotelsearch.data.network.response.search.HotelSearchResponse
 import com.example.hotelsearch.databinding.ActivityMainBinding
 import com.example.hotelsearch.ui.base.ScopedActivity
 import kotlinx.coroutines.launch
@@ -25,28 +27,52 @@ class HotelSearchActivity : ScopedActivity(), KodeinAware {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         viewModel = ViewModelProvider(this, viewModelFactory).get(HotelSearchViewModel::class.java)
-        bindUI()
+        attachListeners()
     }
 
-    private fun bindUI() = launch {
-        val hotels = viewModel.hotellist
+    private fun attachListeners() {
+        binding.ivSearch.setOnClickListener {
+            val searchText = binding.etSearch.text.toString()
+            if (searchText.isEmpty()) {
+                Toast.makeText(this, "Please enter a valid location", Toast.LENGTH_LONG)
+            } else {
+                fetchHotelLocation(searchText)
+            }
+        }
+        locationId.observe(this, Observer {
+            fetchHotelDetails()
+        })
+    }
+
+    private fun fetchHotelLocation(searchText: String) = launch {
+        val hotels = viewModel.getList(searchText)
         hotels.await().observe(this@HotelSearchActivity, Observer {
             binding.progressBar.visibility = View.GONE
             if (it == null) return@Observer
-            Log.d("response", it.toString())
             binding.tvText.text = it.toString()
-            locationId.postValue("2621")
-            //make second call
+            locationId.postValue(getLocationId(it))
         })
+    }
 
-        val hotelDetails = viewModel.hoteldetails
+    private fun fetchHotelDetails() = launch {
+        val hotelDetails = viewModel.getHotelDetails(locationId.value!!)
         hotelDetails.await().observe(this@HotelSearchActivity, Observer {
             if (it == null) return@Observer
             Log.d("response", it.toString())
         })
+    }
 
-
+    private fun getLocationId(response: HotelSearchResponse): String {
+        try {
+            for (h in response.hotelList) {
+                if (h.type.equals("CITY")) {
+                    return h.gaiaId
+                }
+            }
+        } catch (e: java.lang.Exception) {
+            Log.e("error", "city not found")
+        }
+        return "2622"
     }
 }
--- app/src/main/java/com/example/hotelsearch/ui/hotelsearch/HotelSearchViewModel.kt
+++ app/src/main/java/com/example/hotelsearch/ui/hotelsearch/HotelSearchViewModel.kt
@@ -4,23 +4,30 @@ import androidx.lifecycle.ViewModel
 import com.example.hotelsearch.data.network.request.*
 import com.example.hotelsearch.data.repository.HotelsRepository
 import com.example.hotelsearch.internal.lazyDeferred
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 class HotelSearchViewModel(
     private val hotelsRepository: HotelsRepository
 ) : ViewModel() {
 
 
-    val hotellist by lazyDeferred {
-        hotelsRepository.getHotels("new york")
+//    val hotellist by lazyDeferred {
+//        hotelsRepository.getHotels("new york")
+//    }
+
+    fun getList(location: String) = GlobalScope.async(start = CoroutineStart.LAZY) {
+        hotelsRepository.getHotels(location)
     }
 
-    val hoteldetails by lazyDeferred {
+    fun getHotelDetails(regionId: String) = GlobalScope.async(start = CoroutineStart.LAZY) {
         val room = Room(2, listOf(Children(5), Children(7)))
         val request = HotelListRequest(
             CheckInDate(12, 6, 2023),
             CheckOutDate(14, 6, 2023),
             "USD",
-            Destination("2621"),
+            Destination(regionId),
             1,
             "en_US",
             200,
@@ -31,6 +38,24 @@ class HotelSearchViewModel(
         )
         hotelsRepository.getHotelDetails(request)
     }
+
+//    val hoteldetails by lazyDeferred {
+//        val room = Room(2, listOf(Children(5), Children(7)))
+//        val request = HotelListRequest(
+//            CheckInDate(12, 6, 2023),
+//            CheckOutDate(14, 6, 2023),
+//            "USD",
+//            Destination("2621"),
+//            1,
+//            "en_US",
+//            200,
+//            0,
+//            listOf(room),
+//            300000001,
+//            "PRICE_LOW_TO_HIGH"
+//        )
+//        hotelsRepository.getHotelDetails(request)
+//    }
 
 
 }
